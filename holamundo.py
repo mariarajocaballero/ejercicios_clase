a619c925516e5c02820cceccf832696ccd37f6ff
mariarajocaballero <rajo.caballero.maria@iescamas.es>
2024-10-01 10:59:51 +0200

    Add files via upload

---
+++ holamundo.py
@@ -0,0 +1,7 @@
+#Esto es un programa para convertir de euros a dolares
+print("Dime una cantidad")
+num1 = int(input())
+num2 = float(1.10)
+# Realizamos la operación
+total = num1 * num2
+print(" El resultado es: ", total)
