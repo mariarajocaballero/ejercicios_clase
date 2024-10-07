88fb1cacb6f6e1321d597073c9824d23036d9cf4
mariarajocaballero <rajo.caballero.maria@iescamas.es>
2024-10-07 10:11:07 +0200

    Add files via upload

---
+++ ejercicio.py
@@ -0,0 +1,9 @@
+print("Dime la nota")
+nota1 = float(input())
+print("Dima la nota 2")
+nota2 = float(input())
+print(nota2)
+nota3 = float(input())
+print(nota3)
+media = (nota1+nota2+nota3)/3
+print("el resultado de la nota es " , media)
