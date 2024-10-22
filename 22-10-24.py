fe2c5023ff674092e1030001a080e2f22089210f
mariarajocaballero <rajo.caballero.maria@iescamas.es>
2024-10-22 10:53:41 +0200

    Add files via upload

---
+++ 22-10-24.py
@@ -0,0 +1,9 @@
+print("dime el número de la tabla de multiplicar:")
+num_tabla =int(input())
+indice = 1
+while(indice <11):
+    resultado = num_tabla * indice
+    print(indice , "x ", num_tabla, " = ", resultado)
+    indice = indice + 1
+    print("fin")
+
